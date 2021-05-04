f691ecaee63376654c73f932240f6dad365b0025
nsk1512 <nandita.kadam@somaiya.edu>
2021-05-04 20:24:49 +0530

    15

--- accuracy.py
+++ accuracy.py
@@ -22,7 +22,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 #pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
                      #('model', LogisticRegression())])
 
-
 pipeline = Pipeline([('tfidf', TfidfVectorizer(stop_words='english')),
                     ('clf', LinearSVC())])
 
